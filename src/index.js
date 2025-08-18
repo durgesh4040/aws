9d32a17b8e1cc34a13f8d28305bcb32c2f9c1a4d
durgesh <durgesh404011@gmail.com>
2025-08-19 01:23:33 +0530

    aws

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app=express();
+app.use(express.json());
+app.get("/",async (req,res)=>{
+    res.json({
+        msg:" i have deploy my first express app in aws "
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("I have start my server");
+})
